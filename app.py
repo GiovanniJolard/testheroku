628cdeffa7cc6bafea21cf5d54a2013ffe85948a
GiovanniJld <90454077+GiovanniJolard@users.noreply.github.com>
2023-03-02 19:06:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,8 @@ def main():
 # Prédiction de la solvabilité du client
     
     if st.button('Vérifier la solvabilité'):
-        data_pred = data1.loc[id_client].to_numpy().reshape(1, -1)
+        
+        data_pred = data.set_index('SK_ID_CURR').loc[id_client].to_numpy().reshape(1, -1)
         prediction = classifier.predict(data_pred)[0]
         proba = classifier.predict_proba(data_pred)[0, 1]
 
